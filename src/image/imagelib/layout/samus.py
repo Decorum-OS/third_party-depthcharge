764065baa2f3d6264a5b0080abe50727f12a99b9
Gabe Black <gabeblack@google.com>
2016-02-02 16:05:20 -0800

    build_image: Redo selecting a layout, and the refactor the samus layout a bit.
    Layouts are now selected and configured using argparse's subparsers, which has
    several advantages. For instance, he help text is more descriptive, less
    error checking is required on the options detected, and less code is needed in
    the layout to process the extra items.
    
    The samus layout was rearranged a bit to get rid of samus specific paths, tidy
    up how options are consumed and the options that are available, and to move
    from helper functions which construct subsections of the Image to classes
    which embody those subsections.
    
    Change-Id: I649fdf383715062a1d60b93eb4162d7ef1005489

--- src/image/imagelib/layout/samus.py
+++ src/image/imagelib/layout/samus.py
@@ -28,65 +28,53 @@ from imagelib.components.Vblock import Vblock
 from imagelib.util import MB
 from imagelib.util import KB
 
+class VerifiedRw(Index):
+    """The stuff in the RW section that gets verified"""
+    def __init__(self, paths):
+        super(VerifiedRw, self).__init__(
+            File(paths["dc_bin"]), Sha256(File(paths["ec"])),
+            Sha256(File(paths["pd"])), File(paths["ramstage"]),
+            File(paths["refcode"])
+        )
+        self.fill(0xff)
+
+class RwArea(Area):
+    """An RW section of the image"""
+    def __init__(self, paths, model, fmap, suffix):
+        super(RwArea, self).__init__(
+            fmap.section("VBLOCK" + suffix,
+                Vblock(VerifiedRw(paths))
+            ).size(64 * KB),
+            fmap.section("FW_MAIN" + suffix, VerifiedRw(paths)).expand(),
+            Area(
+                fmap.section("PD_MAIN" + suffix,
+                    Index(File(paths["pd"]))
+                ).size(64 * KB),
+                fmap.section("EC_MAIN" + suffix,
+                    Index(File(paths["ec"]))
+                ).expand(),
+                fmap.section("RW_FWID" + suffix,
+                    Fwid(model)
+                ).size(64).fill(0x00)
+            ).size(192 * KB)
+        )
+        self.expand()
+
 class Image(Area):
     model = "Google_Samus"
 
-    def build_verified_rw(self):
-        return Index(
-            File(self.dc_bin), Sha256(File(self.ec_bin)),
-            Sha256(File(self.pd_bin)), File(self.ramstage),
-            File(self.refcode)
-        ).fill(0xff)
-
-    def build_rw(self, fmap, suffix):
-        fmap = self.fmap
-
-        return fmap.section("RW_SECTION" + suffix,
-            fmap.section("VBLOCK" + suffix,
-                Vblock(self.build_verified_rw())
-            ).size(64 * KB),
-            fmap.section("FW_MAIN" + suffix, self.build_verified_rw()).expand(),
-            Area(
-                fmap.section("PD_MAIN" + suffix,
-                    Index(File(self.pd_bin))
-                ).size(64 * KB),
-                fmap.section("EC_MAIN" + suffix,
-                    Index(File(self.ec_bin))
-                ).expand(),
-                fmap.section("RW_FWID" + suffix,
-                    Fwid(self.model)
-                ).size(64).fill(0x00)
-            ).size(192 * KB)
-        ).size(960 * KB)
-
-    def __init__(self, serial, size, gbb_flags=None):
-        self.dc_bin = os.path.join("depthcharge", "depthcharge.payload")
-        self.dc_elf = os.path.join("depthcharge", "depthcharge.elf")
-        self.dtb = os.path.join("dts", "fmap.dts")
-        self.ec_bin = "ec.RW.bin"
-        self.pd_bin = os.path.join("samus_pd", "ec.RW.bin")
-        self.refcode = "refcode.stage"
-        priv_dir = os.path.join("coreboot-private", "3rdparty", "blobs",
-                                "mainboard", "google", "samus")
-        self.ifd = os.path.join(priv_dir, "descriptor.bin")
-        self.me = os.path.join(priv_dir, "me.bin")
-
-        if serial:
-            self.coreboot = "coreboot.rom.serial"
-            self.ramstage = "ramstage.stage.serial"
-            self.legacy = "seabios.cbfs.serial"
-        else:
-            self.coreboot = "coreboot.rom"
-            self.ramstage = "ramstage.stage"
-            self.legacy = "seabios.cbfs"
-
+    def __init__(self, paths, model, size, gbb_flags=None):
         self.fmap = Fmap(size)
         fmap = self.fmap
 
         self.si_bios = fmap.section("SI_BIOS",
             Area(
-                self.build_rw(fmap, "_A"),
-                self.build_rw(fmap, "_B"),
+                fmap.section("RW_SECTION_A",
+                    RwArea(paths, model, fmap, "_A")
+                ).size(960 * KB),
+                fmap.section("RW_SECTION_B",
+                    RwArea(paths, model, fmap, "_B")
+                ).size(960 * KB),
                 fmap.section("RW_MRC_CACHE").size(64 * KB).fill(0xff),
                 fmap.section("RW_ELOG").size(16 * KB).fill(0xff),
                 fmap.section("RW_SHARED",
@@ -98,7 +86,7 @@ class Image(Area):
             ).size(2 * MB),
 
             fmap.section("RW_LEGACY",
-                File(self.legacy)
+                File(paths["legacy"])
             ).size(2 * MB),
 
             fmap.section("WP_RO",
@@ -120,19 +108,19 @@ class Image(Area):
                     fmap.section("BOOT_STUB",
                         Cbfs(
                             CbfsFile(
-                                "u-boot.dtb", File(self.dtb), "mrc_cache"
+                                "u-boot.dtb", File(paths["dtb"]), "mrc_cache"
                             ),
                             CbfsPayload(
-                                "fallback/payload", File(self.dc_elf)
+                                "fallback/payload", File(paths["dc_elf"])
                             ).compression("lzma")
-                        ).base(PartialFile(self.coreboot, 7 * MB, 1 * MB))
+                        ).base(PartialFile(paths["coreboot"], 7 * MB, 1 * MB))
                     ).size(1 * MB)
                 ).expand()
             ).size(2 * MB)
         ).size(6 * MB)
 
-        self.si_desc = fmap.section("SI_DESC", File(self.ifd)).size(4 * KB)
-        self.si_me = fmap.section("SI_ME", File(self.me)).expand()
+        self.si_desc = fmap.section("SI_DESC", File(paths["ifd"])).size(4 * KB)
+        self.si_me = fmap.section("SI_ME", File(paths["me"])).expand()
 
         # This part of the FMAP is odd because it covers two parts of the IFD
         # but not the whole thing. It doesn't sit inside the IFD because that
@@ -159,30 +147,53 @@ class Image(Area):
         self.si_all.place(0, size)
 
 
-def prepare(layout_args):
-    parser = argparse.ArgumentParser()
-
+def add_arguments(parser):
     parser.add_argument('--serial', dest='serial', action='store_true',
                         default=False, help='Enable serial output')
+
+    parser.add_argument('--dev', dest='dev', action='store_true', default=False,
+                        help='Enable developer friendly gbb flags')
 
     parser.add_argument('--size', dest='size', required=True, type=int,
                         help='Size of the image in MB')
 
-    parser.add_argument('--dev-gbb', dest='dev_gbb', action='store_true',
-                        default=False,
-                        help='Enable developer friendly gbb flags')
+    parser.add_argument('--model', dest='model', required=True,
+                        help='Model name to use in firmware IDs')
 
-    options = parser.parse_args(layout_args)
-
+def prepare(options):
     gbb_flags = None
-    if options.dev_gbb:
+    paths = {
+        "dc_bin": os.path.join("depthcharge", "depthcharge.payload"),
+        "dc_elf": os.path.join("depthcharge", "depthcharge.elf"),
+        "dtb": os.path.join("dts", "fmap.dts"),
+        "ec": "ec.RW.bin",
+        "pd": os.path.join("samus_pd", "ec.RW.bin"),
+        "refcode": "refcode.stage",
+        "ifd": "descriptor.bin",
+        "me": "me.bin",
+        "coreboot": "coreboot.rom",
+        "ramstage": "ramstage.stage",
+        "legacy": "seabios.cbfs",
+    }
+
+    if options.dev:
         gbb_flags = (
             Gbb.DevScreenShortDelay |
             Gbb.ForceDevSwitchOn |
             Gbb.ForceDevBootUsb |
             Gbb.DisableFwRollbackCheck
         )
+        paths.update({
+            "dc_bin": os.path.join("depthcharge", "dev.payload"),
+            "dc_elf": os.path.join("depthcharge", "dev.elf"),
+        })
 
-    return Image(serial=options.serial,
-                 size=options.size * MB,
+    if options.serial:
+        paths.update({
+            "coreboot": "coreboot.rom.serial",
+            "ramstage": "ramstage.stage.serial",
+            "legacy": "seabios.cbfs.serial",
+        })
+
+    return Image(paths=paths, model=options.model, size=options.size * MB,
                  gbb_flags=gbb_flags)
