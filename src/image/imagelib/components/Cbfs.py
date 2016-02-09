eb9db28fe5d0b6685160c2250ea0edba7cb7b451
Gabe Black <gabeblack@google.com>
2016-02-08 22:21:12 -0800

    build_image: Refactor Cbfs and specialize the logging.
    Create a CbfsFileBase class and fold a lot of functionality into it. Also
    specialize the logging of the File, Payload, and main Cbfs classes. In the case
    of the payload and file classes, log various additional properties if they've
    been set, and also put the CBFS file name into the Area's "name".
    
    Also, treat the bootblock as a child object, and use Area as the base class
    for the file and payload classes. The Cbfs Area is derived, but the file and
    payload Areas aren't, even though they don't get pasted directly into the top
    level output file.
    
    For the Cbfs area itself, add in extra properties, and add child properties
    either the base or bootblock, if one was provided, and then log a list of the
    files and/or payload which are included.
    
    Change-Id: I16a337ea9241d43bf207333d402614c6bbd481be

--- src/image/imagelib/components/Cbfs.py
+++ src/image/imagelib/components/Cbfs.py
@@ -12,71 +12,62 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from Area import DerivedArea
+from Area import Area, DerivedArea
 from imagelib.tools.Cbfstool import Cbfstool
+from imagelib.tools.Tool import FileHarness
 
-import os
-import tempfile
-
-class CbfsFile(DerivedArea):
-    def __init__(self, name, data, t):
-        super(CbfsFile, self).__init__(data)
-        self._name = name
-        self._data = data
-        self._t = t
-        self._base = None
-
-        self.shrink()
-
-    def compute_min_size_content(self):
-        if not self.handle_children():
-            self._data_buf = self._data.write()
-        return len(self._data_buf)
-
-    def base(self, base):
-        self._base = base
-        return self
-
-    def install(self, cbfstool):
-        h, path = tempfile.mkstemp()
-        with os.fdopen(h, "w+b") as f:
-            f.write(self._data_buf)
-        cbfstool.add(path, self._name, self._t, self._base)
-        os.remove(path)
-
-class CbfsPayload(DerivedArea):
+class CbfsFileBase(Area):
     def __init__(self, name, data):
-        super(CbfsPayload, self).__init__(data)
+        super(CbfsFileBase, self).__init__(data)
         self._name = name
         self._data = data
+        self._type = None
         self._base = None
         self._compression = None
 
         self.shrink()
 
-    def compute_min_size_content(self):
-        if not self.handle_children():
-            self._data_buf = self._data.write()
-        return len(self._data_buf)
-
     def base(self, base):
         self._base = base
         return self
 
+    def log_area_name(self):
+        name = super(CbfsFileBase, self).log_area_name()
+        return name + "[{}]".format(self._name)
+
+    def log_get_additional_properties(self):
+        props = []
+        if self._type is not None:
+            props.append("type={}".format(self._type))
+        if self._base is not None:
+            props.append("base={:#x}".format(self._base))
+        if self._compression is not None:
+            props.append("compression={}".format(self._compression))
+        return props
+
+class CbfsFile(CbfsFileBase):
+    def __init__(self, name, data, cbfs_type):
+        super(CbfsFile, self).__init__(name, data)
+        self._type = cbfs_type
+
+    def install(self, cbfstool):
+        with FileHarness(self._data.write()) as [data]:
+            cbfstool.add(data, self._name, self._type, self._base)
+
+class CbfsPayload(CbfsFileBase):
     def compression(self, compression):
         self._compression = compression
         return self
 
     def install(self, cbfstool):
-        h, path = tempfile.mkstemp()
-        with os.fdopen(h, "w+b") as f:
-            f.write(self._data_buf)
-        cbfstool.add_payload(path, self._name, self._compression, self._base)
-        os.remove(path)
+        with FileHarness(self._data.write()) as [data]:
+            cbfstool.add_payload(data, self._name, self._compression,
+                                 self._base)
 
 class Cbfs(DerivedArea):
     def __init__(self, *args):
         super(Cbfs, self).__init__(*args)
+        self._files = args
         self._base = None
         self._bootblock = None
         self._arch = None
@@ -91,6 +82,7 @@ class Cbfs(DerivedArea):
 
     def bootblock(self, bootblock):
         self._bootblock = bootblock
+        self.add_child(bootblock)
         return self
 
     def arch(self, arch):
@@ -106,21 +98,49 @@ class Cbfs(DerivedArea):
         return self._base.computed_min_size if self._base else 0
 
     def write(self):
-        h, path = tempfile.mkstemp()
-        with os.fdopen(h, "w+b") as f:
+        blobs = []
+        if self._base:
+            blobs.append(self._base.write())
+        else:
+            blobs.append(None)
+            if not self._bootblock:
+                raise ValueError("No bootblock specified")
+            blobs.append(self._bootblock.write())
+
+        with FileHarness(*blobs) as files:
             if self._base:
-                f.write(self._base.write())
-                cbfstool = Cbfstool(path)
+                [base] = files
+                cbfstool = Cbfstool(base)
             else:
+                base, bootblock = files
                 cbfstool = Cbfstool()
-                if not self._bootblock:
-                    raise ValueError("No bootblock specified")
-                cbfstool.create(path, self.placed_size, self._bootblock,
+                cbfstool.create(base, self.placed_size, bootblock,
                                 self._arch, self._align, self._offset)
-            for child in self.children:
-                if child is not self._base:
-                    child.install(cbfstool)
-            f.seek(0)
-            buf = f.read()
-        os.remove(path)
-        return buf
+            for f in self._files:
+                f.install(cbfstool)
+            with open(base, "rb") as data:
+                return data.read()
+
+    def log_get_additional_properties(self):
+        props = []
+        if self._arch is not None:
+            props.append("arch={}".format(self._arch))
+        if self._align is not None:
+            props.append("align={}".format(self._align))
+        if self._offset is not None:
+            props.append("offset={}".format(self._offset))
+        return props
+
+    def log_area_content(self, indent):
+        child_props = {}
+        if self._base is not None:
+            child_props["Base"] = self._base
+        if self._bootblock is not None:
+            child_props["Boot block"] = self._bootblock
+        content = self.log_child_props(indent, child_props)
+
+        if len(self._files):
+            content += "\n\n{}Files:\n{}".format(
+                indent, "\n".join(f.log_area(indent) for f in self._files))
+
+        return content
