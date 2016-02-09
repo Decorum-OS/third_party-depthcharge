fe0452f17d469125d213306436b7f63ac1b64c9f
Gabe Black <gabeblack@google.com>
2016-02-08 22:14:32 -0800

    build_image: Use FileHarness and specialize logging in Gbb.
    Also, the GBB flags are handled a little bit differently now. It adds an
    AllFlags dictionary which maps flag names to their values, and processes it
    to add individual members for each of the flags.
    
    Change-Id: I8d63a23384aefd07efbccbbe684bcc83ee0f6d41

--- src/image/imagelib/components/Gbb.py
+++ src/image/imagelib/components/Gbb.py
@@ -15,24 +15,26 @@
 from Area import DerivedArea
 from File import File
 from imagelib.tools.GbbUtility import GbbUtility
-
-import os
-import tempfile
+from imagelib.tools.Tool import FileHarness
 
 class Gbb(DerivedArea):
-    DevScreenShortDelay = 0x00000001
-    LoadOptionRoms = 0x00000002
-    EnableAlternateOs = 0x00000004
-    ForceDevSwitchOn = 0x00000008
-    ForceDevBootUsb = 0x00000010
-    DisableFwRollbackCheck = 0x00000020
-    EnterTriggersTonorm = 0x00000040
-    ForceDevBootLegacy = 0x00000080
-    FaftKeyOverride = 0x00000100
-    DisableEcSoftwareSync = 0x00000200
-    DefaultDevBootLegacy = 0x00000400
-    DisablePdSoftwareSync = 0x00000800
-    ForceDevBootFastbootFullCap = 0x00002000
+    # The flags in this dictionary will be turned into independent members of
+    # the class programatically after the class definition is complete.
+    AllFlags = {
+        "DevScreenShortDelay": 0x00000001,
+        "LoadOptionRoms": 0x00000002,
+        "EnableAlternateOs": 0x00000004,
+        "ForceDevSwitchOn": 0x00000008,
+        "ForceDevBootUsb": 0x00000010,
+        "DisableFwRollbackCheck": 0x00000020,
+        "EnterTriggersTonorm": 0x00000040,
+        "ForceDevBootLegacy": 0x00000080,
+        "FaftKeyOverride": 0x00000100,
+        "DisableEcSoftwareSync": 0x00000200,
+        "DefaultDevBootLegacy": 0x00000400,
+        "DisablePdSoftwareSync": 0x00000800,
+        "ForceDevBootFastbootFullCap": 0x00002000,
+    }
 
 
     def __init__(self, hwid, flags=None, bmpfv=None,
@@ -56,29 +58,29 @@ class Gbb(DerivedArea):
         super(Gbb, self).__init__(bmpfv, rootkey, recoverykey)
 
     def write(self):
-        gbb, gbbp = tempfile.mkstemp()
-        bmpfv, bmpfvp = tempfile.mkstemp()
-        rootkey, rootkeyp = tempfile.mkstemp()
-        recoverykey, recoverykeyp = tempfile.mkstemp()
-        gbb, bmpfv, rootkey, recoverykey = \
-            (os.fdopen(f, "w+b") for f in (gbb, bmpfv, rootkey, recoverykey))
-
         gbb_utility = GbbUtility()
-        gbb_utility.create([0x100, 0x1000, self.placed_size - 0x2180, 0x1000],
-                           gbbp)
+        with FileHarness(None, self._bmpfv.write(), self._rootkey.write(),
+                         self._recoverykey.write()) as files:
+            gbb, bmpfv, rootkey, recoverykey = files
+            sizes = [0x100, 0x1000, self.placed_size - 0x2180, 0x1000]
+            gbb_utility.create(sizes, gbb)
+            gbb_utility.set(self._hwid, self._flags, bmpfv,
+                            rootkey, recoverykey)
 
-        bmpfv.write(self._bmpfv.write())
-        rootkey.write(self._rootkey.write())
-        recoverykey.write(self._recoverykey.write())
-        for f in bmpfv, rootkey, recoverykey:
-            f.close()
-        gbb_utility.set(self._hwid, self._flags, bmpfvp,
-                        rootkeyp, recoverykeyp)
+            with open(gbb, "rb") as data:
+                return data.read()
 
-        buf = gbb.read()
-        gbb.close()
+    def log_get_additional_properties(self):
+        return [name for name, val in Gbb.AllFlags.iteritems()
+                if val & self._flags]
 
-        for path in gbbp, bmpfvp, rootkeyp, recoverykeyp:
-            os.remove(path)
+    def log_area_content(self, indent):
+        child_areas = {
+            "Bitmap block": self._bmpfv,
+            "Root key": self._rootkey,
+            "Recovery key": self._recoverykey,
+        }
+        return self.log_child_props(indent, child_areas)
 
-        return buf
+for name, val in Gbb.AllFlags.iteritems():
+    setattr(Gbb, name, val)
