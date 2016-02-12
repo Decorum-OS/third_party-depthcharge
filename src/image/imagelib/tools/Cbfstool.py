7ce662993ced8203190274862261d23c158c999d
Gabe Black <gabeblack@google.com>
2016-02-11 17:15:45 -0800

    build_image: Make some variable names in Cbfstool a bit more descriptive.
    They had been named based on the short form command line arguments cbfstool
    accepts, rather than what they are actually used for.
    
    Change-Id: I0dd5514d48ade69cce8a3d91da22152df4d556d4

--- src/image/imagelib/tools/Cbfstool.py
+++ src/image/imagelib/tools/Cbfstool.py
@@ -33,17 +33,17 @@ class Cbfstool(Tool):
             raise RuntimeError("cbfstool create failed:\n%s" % stdout)
         self.base = name
 
-    def add(self, f, name, t, base=None):
-        args = [self.cmd, self.base, "add", "-f", f, "-n", name,
-                "-t", str(t)]
+    def add(self, name, cbfs_name, cbfs_type, base=None):
+        args = [self.cmd, self.base, "add", "-f", name, "-n", cbfs_name,
+                "-t", str(cbfs_type)]
         if base is not None:
             args.extend(["-b", "%d"])
         ret, stdout = self.run(args)
         if ret != 0:
             raise RuntimeError("cbfstool add failed:\n%s" % stdout)
 
-    def add_payload(self, f, name, compression=None, base=None):
-        args = [self.cmd, self.base, "add-payload", "-f", f, "-n", name]
+    def add_payload(self, name, cbfs_name, compression=None, base=None):
+        args = [self.cmd, self.base, "add-payload", "-f", name, "-n", cbfs_name]
         if compression is not None:
             args.extend(["-c", compression])
         if base is not None:
