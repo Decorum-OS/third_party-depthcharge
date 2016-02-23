717fb6bae7b15221e0ea6fb6b77d3f5dd9113a2e
Gabe Black <gabeblack@google.com>
2016-02-22 19:30:59 -0800

    build_image: Rework the samus config to use dcdir.
    This is in place of CBFS, FMAP and the Index structure. The alignment of the
    dcdir structures aren't guaranteed as mentioned in another change.
    
    The legacy binary is stored in a CBFS and is slightly too big to fit in the
    allocated space because of dcdir directory overhead. The legacy area could be
    a raw region with no overhead, but it seems like a better idea to make
    allowances for storing more than one blob of data in that area, and to be able
    to show how much of the space is occupied at that moment instead of how much
    space was allocated absolutely.
    
    Coreboot's ramstage is still included in the image just to fill it out and
    will be removed in the future.
    
    Change-Id: I536808c41ca2edd6055e2863819a9894762a1e2e

--- src/image/imagelib/layout/samus.py
+++ src/image/imagelib/layout/samus.py
@@ -16,132 +16,76 @@ import argparse
 import os
 
 from imagelib.components.Area import Area
-from imagelib.components.Cbfs import Cbfs, CbfsPayload, CbfsFile
-from imagelib.components.File import File, PartialFile
-from imagelib.components.Fmap import Fmap
+from imagelib.components.Dcdir import \
+    Region, Directory, RootDirectory, DirectoryTable
+from imagelib.components.File import File
 from imagelib.components.Fwid import Fwid
 from imagelib.components.Gbb import Gbb
 from imagelib.components.Ifd import Ifd
-from imagelib.components.Index import Index
 from imagelib.components.Sha256 import Sha256
 from imagelib.components.Vblock import Vblock
 from imagelib.util import MB
 from imagelib.util import KB
 
-class VerifiedRw(Index):
+class VerifiedRw(Directory):
     """The stuff in the RW section that gets verified"""
-    def __init__(self, paths):
-        super(VerifiedRw, self).__init__(
-            File(paths["dc_bin"]), Sha256(File(paths["ec"])),
-            Sha256(File(paths["pd"])), File(paths["ramstage"]),
-            File(paths["refcode"])
+    def __init__(self, paths, name=None):
+        super(VerifiedRw, self).__init__(name,
+            Region("MAIN", File(paths["dc_bin"])).shrink(),
+            Region("EC_HASH", Sha256(File(paths["ec"]))).shrink(),
+            Region("PD_HASH", Sha256(File(paths["pd"]))).shrink(),
+            Region("RAMSTAGE", File(paths["ramstage"])).shrink(),
+            Region("REFCODE", File(paths["refcode"])).shrink()
         )
-        self.fill(0xff)
+        self.shrink()
 
-class RwArea(Area):
+class RwArea(Directory):
     """An RW section of the image"""
-    def __init__(self, paths, model, fmap, suffix):
-        super(RwArea, self).__init__(
-            fmap.section("VBLOCK" + suffix,
-                Vblock(VerifiedRw(paths))
-            ).size(64 * KB),
-            fmap.section("FW_MAIN" + suffix, VerifiedRw(paths)).expand(),
-            Area(
-                fmap.section("PD_MAIN" + suffix,
-                    Index(File(paths["pd"]))
-                ).size(64 * KB),
-                fmap.section("EC_MAIN" + suffix,
-                    Index(File(paths["ec"]))
-                ).expand(),
-                fmap.section("RW_FWID" + suffix,
-                    Fwid(model)
-                ).size(64).fill(0x00)
-            ).size(192 * KB)
+    def __init__(self, name, paths, model):
+        super(RwArea, self).__init__(name,
+            Region("VBLOCK", Vblock(VerifiedRw(paths))).size(64 * KB),
+            VerifiedRw(paths, "VERIFIED").expand(),
+            Region("PD", File(paths["pd"])).shrink(),
+            Region("EC", File(paths["ec"])).shrink(),
+            Region("FWID", Fwid(model)).size(64).fill(0x00)
         )
         self.expand()
 
-class Image(Area):
+class Image(RootDirectory):
     model = "Google_Samus"
 
     def __init__(self, paths, model, size, gbb_flags=None):
-        self.fmap = Fmap(size)
-        fmap = self.fmap
+        si_bios = Area(
+            Directory("RW",
+                Directory("LEGACY").size(2 * MB),
+                Region("MRCCACHE").size(64 * KB),
+                Region("ELOG").size(16 * KB),
+                Directory("SCRATCH").size(16 * KB),
+                Region("VPD").size(8 * KB),
+                RwArea("A", paths, model).expand(),
+                RwArea("B", paths, model).expand()
+            ).size(size / 2),
+            DirectoryTable(),
+            Directory("RO",
+                Region("GBB",
+                    Gbb(hwid="SAMUS TEST 8028", flags=gbb_flags).expand()
+                ).expand(),
+                Region("VPD").size(16 * KB),
+                Region("FWID", Fwid(self.model)).size(64).fill(0x00),
+                Directory("BOOTSTUB").size(1 * MB)
+            ).expand(),
+        ).expand()
 
-        self.si_bios = fmap.section("SI_BIOS",
-            Area(
-                fmap.section("RW_SECTION_A",
-                    RwArea(paths, model, fmap, "_A")
-                ).size(960 * KB),
-                fmap.section("RW_SECTION_B",
-                    RwArea(paths, model, fmap, "_B")
-                ).size(960 * KB),
-                fmap.section("RW_MRC_CACHE").size(64 * KB).fill(0xff),
-                fmap.section("RW_ELOG").size(16 * KB).fill(0xff),
-                fmap.section("RW_SHARED",
-                    fmap.section("SHARED_DATA").size(8 * KB).fill(0x00),
-                    fmap.section("VBLOCK_DEV").size(8 * KB).fill(0xff)
-                ).size(16 * KB),
-                fmap.section("RW_VPD").size(8 * KB).fill(0xff),
-                fmap.section("RW_UNUSED").expand().fill(0xff),
-            ).size(2 * MB),
+        si_me = Region("ME", File(paths["me"])).expand()
+        si_desc = Region("IFD_DESC", File(paths["ifd"])).expand()
 
-            fmap.section("RW_LEGACY",
-                File(paths["legacy"])
-            ).size(2 * MB),
-
-            fmap.section("WP_RO",
-                Area(
-                    fmap.section("RO_VPD").size(16 * KB).fill(0xff),
-                    fmap.section("RO_UNUSED").expand().fill(0xff)
-                ).size(64 * KB),
-                fmap.section("RO_SECTION",
-                    Area(
-                        fmap.section("FMAP", fmap).size(2 * KB),
-                        fmap.section("RO_FRID",
-                            Fwid(self.model)
-                        ).size(64).fill(0x00),
-                        fmap.section("RO_FRID_PAD").expand().fill(0xff)
-                    ).size(4 * KB),
-                    fmap.section("GBB",
-                        Gbb(hwid="SAMUS TEST 8028", flags=gbb_flags).expand()
-                    ).expand(),
-                    fmap.section("BOOT_STUB",
-                        Cbfs(
-                            CbfsPayload(
-                                "fallback/payload", File(paths["dc_elf"])
-                            ).compression("lzma")
-                        ).base(PartialFile(paths["coreboot"], 7 * MB, 1 * MB))
-                    ).size(1 * MB)
-                ).expand()
-            ).size(2 * MB)
-        ).size(6 * MB)
-
-        self.si_desc = fmap.section("SI_DESC", File(paths["ifd"])).size(4 * KB)
-        self.si_me = fmap.section("SI_ME", File(paths["me"])).expand()
-
-        # This part of the FMAP is odd because it covers two parts of the IFD
-        # but not the whole thing. It doesn't sit inside the IFD because that
-        # would imply it sits within one of it's regions, and it doesn't sit
-        # outside of the IFD because it doesn't cover all of it. It will have
-        # to be handled specially in the Image's "place" function.
-        self.si_all = fmap.section("SI_ALL")
-
-        ifd = Ifd(self.si_desc).expand()
-        ifd.region("me", self.si_me)
-        ifd.region("bios", self.si_bios)
+        ifd = Ifd(si_desc).expand()
+        ifd.region("me", si_me)
+        ifd.region("bios", si_bios)
 
         super(Image, self).__init__(ifd)
+        self.big_pointer()
         self.size(size)
-        self.fill(0x00)
-
-    def place(self, offset, size):
-        super(Image, self).place(offset, size)
-
-        # Manually place the "SI_ALL" fmap region over the IFD descriptor and
-        # ME region.
-        offset = self.si_desc.placed_offset
-        size = self.si_desc.placed_size + self.si_me.placed_size
-        self.si_all.place(0, size)
 
 
 def add_arguments(parser):
@@ -165,15 +109,12 @@ def prepare(options):
     gbb_flags = None
     paths = {
         "dc_bin": "cb_payload.payload",
-        "dc_elf": "cb_payload.elf",
         "ec": "ec.RW.bin",
         "pd": os.path.join("samus_pd", "ec.RW.bin"),
         "refcode": "refcode.stage",
         "ifd": "descriptor.bin",
         "me": "me.bin",
-        "coreboot": "coreboot.rom",
         "ramstage": "ramstage.stage",
-        "legacy": "seabios.cbfs",
     }
 
     if options.dev or options.netboot:
@@ -187,20 +128,16 @@ def prepare(options):
     if options.dev:
         paths.update({
             "dc_bin": "cb_dev.payload",
-            "dc_elf": "cb_dev.elf",
         })
 
     if options.netboot:
         paths.update({
             "dc_bin": "netboot.payload",
-            "dc_elf": "netboot.elf",
         })
 
     if options.serial:
         paths.update({
-            "coreboot": "coreboot.rom.serial",
             "ramstage": "ramstage.stage.serial",
-            "legacy": "seabios.cbfs.serial",
         })
 
     return Image(paths=paths, model=options.model, size=options.size * KB,
