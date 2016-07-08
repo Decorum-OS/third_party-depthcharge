0ea65202ea631b3a2039f28882211366a5338ea6
Gabe Black <gabeblack@google.com>
2016-07-08 01:32:48 -0700

    storage: Add a driver for dcdir based storage.
    Change-Id: Iadd05d135e146970ebcf2ec85c8d0913bc4f4826

---
+++ src/drivers/storage/dcdir.c
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+
+#include "base/xalloc.h"
+#include "base/container_of.h"
+#include "base/dcdir.h"
+#include "drivers/storage/dcdir.h"
+
+static int dcdir_storage_init_root(DcDirStorageRoot *root)
+{
+	if (dcdir_open_root(&root->root_handle, root->media,
+			    root->anchor_offset)) {
+		return 1;
+	}
+
+	root->opened = 1;
+	return 0;
+}
+
+static int dcdir_storage_root_init_dir(DcDirStorageOps *me, StorageOps **media,
+				       DcDirStorageDir *dir)
+{
+	DcDirStorageRoot *root = container_of(me, DcDirStorageRoot, ops);
+	*media = root->media;
+
+	if (!root->opened && dcdir_storage_init_root(root))
+		return 1;
+
+	if (dcdir_open_dir(&dir->dir_handle, *media, &root->root_handle,
+			   dir->name)) {
+		return 1;
+	}
+
+	return 0;
+}
+
+static int dcdir_storage_root_init_region(DcDirStorageOps *me,
+					  StorageOps **media,
+					  DcDirStorage *region)
+{
+	DcDirStorageRoot *root = container_of(me, DcDirStorageRoot, ops);
+	*media = root->media;
+
+	if (!root->opened && dcdir_storage_init_root(root))
+		return 1;
+
+	if (dcdir_open_region(&region->region_handle, *media,
+			      &root->root_handle, region->name)) {
+		return 1;
+	}
+
+	return 0;
+}
+
+DcDirStorageRoot *new_dcdir_storage_root(StorageOps *media,
+					 uint32_t anchor_offset)
+{
+	DcDirStorageRoot *root = xzalloc(sizeof(*root));
+
+	root->ops.open_dir = &dcdir_storage_root_init_dir;
+	root->ops.open_region = &dcdir_storage_root_init_region;
+
+	root->media = media;
+	root->anchor_offset = anchor_offset;
+
+	return root;
+}
+
+
+
+static int dcdir_storage_init_dir(DcDirStorageDir *dir, StorageOps **media)
+{
+	if (dir->parent->open_dir(dir->parent, media, dir))
+		return 1;
+
+	dir->opened = 1;
+	return 0;
+}
+
+static int dcdir_storage_dir_init_dir(DcDirStorageOps *me, StorageOps **media,
+				      DcDirStorageDir *dir)
+{
+	DcDirStorageDir *this_dir = container_of(me, DcDirStorageDir, ops);
+
+	if (!this_dir->opened && dcdir_storage_init_dir(this_dir, media))
+		return 1;
+
+	if (dcdir_open_dir(&dir->dir_handle, *media, &this_dir->dir_handle,
+			   dir->name)) {
+		return 1;
+	}
+
+	return 0;
+}
+
+static int dcdir_storage_dir_init_region(DcDirStorageOps *me,
+					 StorageOps **media,
+					 DcDirStorage *region)
+{
+	DcDirStorageDir *this_dir = container_of(me, DcDirStorageDir, ops);
+
+	if (!this_dir->opened && dcdir_storage_init_dir(this_dir, media))
+		return 1;
+
+	if (dcdir_open_region(&region->region_handle, *media,
+			      &this_dir->dir_handle, region->name)) {
+		return 1;
+	}
+
+	return 0;
+}
+
+DcDirStorageDir *new_dcdir_storage_dir(DcDirStorageOps *parent,
+				       const char *name)
+{
+	DcDirStorageDir *dir = xzalloc(sizeof(*dir));
+
+	dir->ops.open_dir = &dcdir_storage_dir_init_dir;
+	dir->ops.open_region = &dcdir_storage_dir_init_region;
+
+	dir->parent = parent;
+	dir->name = name;
+
+	return dir;
+}
+
+
+
+
+static int dcdir_storage_init(DcDirStorage *storage, StorageOps **media)
+{
+	return storage->parent->open_region(storage->parent, media, storage);
+}
+
+static int dcdir_storage_read(StorageOps *me, void *buffer,
+			      uint64_t offset, size_t size)
+{
+	DcDirStorage *storage = container_of(me, DcDirStorage, ops);
+
+	if (!storage->media && dcdir_storage_init(storage, &storage->media))
+		return 1;
+
+	if (offset + size > storage->region_handle.size) {
+		printf("Read beyond the bounds of dcdir region.\n");
+		return 1;
+	}
+
+	return storage_read(storage->media, buffer,
+			    storage->region_handle.offset + offset, size);
+}
+
+static int dcdir_storage_write(StorageOps *me, const void *buffer,
+			       uint64_t offset, size_t size)
+{
+	DcDirStorage *storage = container_of(me, DcDirStorage, ops);
+
+	if (!storage->media && dcdir_storage_init(storage, &storage->media))
+		return 1;
+
+	if (offset + size > storage->region_handle.size) {
+		printf("Read beyond the bounds of dcdir region.\n");
+		return 1;
+	}
+
+	return storage_write(storage->media, buffer,
+			     storage->region_handle.offset + offset, size);
+}
+
+DcDirStorage *new_dcdir_storage(DcDirStorageOps *parent, const char *name)
+{
+	DcDirStorage *storage = xzalloc(sizeof(*storage));
+
+	storage->ops.read = &dcdir_storage_read;
+	storage->ops.write = &dcdir_storage_write;
+
+	storage->parent = parent;
+	storage->name = name;
+
+	return storage;
+}
---
+++ src/drivers/storage/dcdir.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __DRIVERS_STORAGE_DCDIR_H__
+#define __DRIVERS_STORAGE_DCDIR_H__
+
+#include <stdint.h>
+
+#include "base/dcdir.h"
+#include "drivers/storage/storage.h"
+
+struct DcDirStorageDir;
+struct DcDirStorage;
+
+typedef struct DcDirStorageOps {
+	int (*open_dir)(struct DcDirStorageOps *me, StorageOps **media,
+			struct DcDirStorageDir *dir);
+	int (*open_region)(struct DcDirStorageOps *me, StorageOps **media,
+			   struct DcDirStorage *region);
+} DcDirStorageOps;
+
+typedef struct {
+	DcDirStorageOps ops;
+
+	StorageOps *media;
+	uint32_t anchor_offset;
+
+	DcDir root_handle;
+	int opened;
+} DcDirStorageRoot;
+
+struct DcDirStorageDir {
+	DcDirStorageOps ops;
+
+	DcDirStorageOps *parent;
+	const char *name;
+
+	DcDir dir_handle;
+	int opened;
+};
+typedef struct DcDirStorageDir DcDirStorageDir;
+
+struct DcDirStorage {
+	StorageOps ops;
+
+	DcDirStorageOps *parent;
+	const char *name;
+
+	StorageOps *media;
+	DcDirRegion region_handle;
+};
+typedef struct DcDirStorage DcDirStorage;
+
+DcDirStorageRoot *new_dcdir_storage_root(StorageOps *media,
+					 uint32_t anchor_offset);
+DcDirStorageDir *new_dcdir_storage_dir(DcDirStorageOps *parent,
+				       const char *name);
+DcDirStorage *new_dcdir_storage(DcDirStorageOps *parent, const char *name);
+
+#endif /* __DRIVERS_STORAGE_DCDIR_H__ */
