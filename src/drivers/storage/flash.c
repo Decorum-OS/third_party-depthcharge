1d1f3e510869438c39f8a2fba552dad827d76492
Gabe Black <gabeblack@google.com>
2016-07-11 19:30:59 -0700

    storage: Implement the flash storage write callback.
    This will check to see if it's necessary to erase, and also do a read/modify
    write on any sectors that do need to be erased and which are only being
    partially overwritten.
    
    I don't have hardware which would actually write to flash since that's not
    used in depthcharge except on a particular machine. It will be used at some
    point on samus, but I'll have to reintroduce the PCH SPI driver first which
    I'm not ready to do yet.
    
    Change-Id: Ica8dfefc1fbe9d87291ed3e8c2f20a41bda500b5

--- src/drivers/storage/flash.c
+++ src/drivers/storage/flash.c
@@ -20,8 +20,11 @@
  * MA 02111-1307 USA
  */
 
+#include <assert.h>
+#include <stdio.h>
 #include <string.h>
 
+#include "base/algorithm.h"
 #include "base/container_of.h"
 #include "base/die.h"
 #include "base/xalloc.h"
@@ -43,7 +46,90 @@ static int flash_storage_read(StorageOps *me, void *buffer,
 static int flash_storage_write(StorageOps *me, const void *buffer,
 			       uint64_t offset, size_t size)
 {
-	die("Flash storage writes not implemented!");
+	FlashStorage *storage = container_of(me, FlashStorage, ops);
+	if (offset + size > storage->flash->size(storage->flash)) {
+		printf("Write beyond end of flash storage.\n");
+		return 1;
+	}
+
+	const int64_t erase_size = storage->flash->erase_size(storage->flash);
+	if (erase_size < 0)
+		return 1;
+	// Make sure the erase size is a power of 2.
+	assert(((erase_size - 1) & erase_size) == 0);
+	const uint64_t erase_mask = ~((uint64_t)erase_size - 1);
+
+	// Round the original offset down to an erase boundary.
+	uint64_t aligned_offset = offset & erase_mask;
+	// Calculate how much below the original offset that is.
+	uint64_t extra_low = offset - aligned_offset;
+	// Add that into the original size.
+	uint64_t aligned_size = size + extra_low;
+	// Now round the size up so that it's a multiple of the erase size.
+	aligned_size = (aligned_size + ~erase_mask) & erase_mask;
+
+	uint64_t input_pos = 0;
+
+	// Process one flash sector at a time.
+	for (uint64_t pos = aligned_offset;
+	     pos < aligned_offset + aligned_size;
+	     pos += erase_size) {
+
+		// Read the existing contents.
+		uint8_t *existing =
+			storage->flash->read(storage->flash, pos, erase_size);
+		if (!existing)
+			return 1;
+		// Set up a pointer to data to write to this sector.
+		uint8_t *new_data = (uint8_t *)buffer + input_pos;
+		// Figure out how many bytes long that is.
+		int bytes = MIN(erase_size - extra_low, size - input_pos);
+
+		// Loop until we would hit the end of either buffer.
+		for (int i = 0; i < bytes; i++) {
+			// We'll assume bits can change from 1 to 0 but not the
+			// other way.
+			uint8_t original = existing[i + extra_low];
+			uint8_t new = new_data[i];
+			if ((original & new) != new) {
+				// A straight overwrite isn't possible, so
+				// we need to erase this sector.
+				if (storage->flash->erase(storage->flash,
+							  pos, erase_size))
+					return 1;
+				// We already erased, so we can stop now.
+				break;
+			}
+		}
+
+		// Prepare the chunk of data to write.
+		memcpy(existing + extra_low, new_data, bytes);
+		uint8_t *write_buf = existing;
+		int write_size = erase_size;
+		uint64_t write_pos = pos;
+
+		// We can skip over bytes that are all ones on either end.
+		while (write_size && write_buf[write_size - 1] == 0xff)
+			write_size--;
+		while (write_size && write_buf[0] == 0xff) {
+			write_buf++;
+			write_size--;
+			write_pos++;
+		}
+
+		// Actually do the write.
+		if (storage->flash->write(storage->flash, write_buf,
+					  write_pos, write_size)) {
+			return 1;
+		}
+
+		// Update our position in the input buffer.
+		input_pos += bytes;
+		// The start is no longer misaligned.
+		extra_low = 0;
+	}
+
+	return 0;
 }
 
 static int flash_storage_size(StorageOps *me)
