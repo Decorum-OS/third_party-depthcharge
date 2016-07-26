fb4de07cecb2501448404fb1f6ea228bc1579bec
Gabe Black <gabeblack@google.com>
2016-07-25 22:14:37 -0700

    elf: Fix the definition of the 64 bit segment header.
    This structure had its elements in the same order as they are in the 32 bit
    version, but for the 64 bit version they've been reordered. Presumably that
    was to naturally align the expanded members.
    
    Change-Id: I112e148033854f045ddab1c9648cd8617ebcf7bc

--- src/base/libc/includes/elf.h
+++ src/base/libc/includes/elf.h
@@ -120,12 +120,12 @@ typedef struct {
 
 typedef struct {
 	uint32_t p_type;
-	uint32_t p_offset;
+	uint32_t p_flags;
+	uint64_t p_offset;
 	uint64_t p_vaddr;
 	uint64_t p_paddr;
 	uint64_t p_filesz;
 	uint64_t p_memsz;
-	uint64_t p_flags;
 	uint64_t p_align;
 } Elf64_Phdr;
 
